028ecd841614a6c7a381bfe5fa2f7f56bb0a3a4f
Shaw <502214125@qq.com>
2019-06-10 22:04:50 +0800

    定时器6进中断和定时器4一样的初始化也不行， 先到这了，

--- User/main.c
+++ User/main.c
@@ -22,9 +22,10 @@ void start_task(void *pvParameters);//任务函数
 TaskHandle_t INTERRUPTTask_Handler;     //任务句柄
 void interrupt_task(void *p_arg);       //任务函数
 
-void TIM3_Int_Init(u16 arr, u16 pac);
+void TIM2_Int_Init(u16 arr, u16 pac);
 void TIM4_Int_Init(u16 arr, u16 pac);
 void TIM5_Int_Init(u16 arr, u16 pac);
+void TIM6_Int_Init(u16 arr, u16 pac);
 
 
 int main(void)
@@ -35,9 +36,10 @@ int main(void)
 	USART1_init(115200);
     delay_init(); 
     led_init();
-	TIM3_Int_Init(1000-1, 7200-1);              //初始化定时器3， 定时器周期1S
-	TIM4_Int_Init(1000-1, 7200-1);              //初始化定时器5， 定时器周期1S
+	TIM2_Int_Init(1000-1, 7200-1);              //初始化定时器2， 定时器周期1S
+	TIM4_Int_Init(1000-1, 7200-1);              //初始化定时器4， 定时器周期1S
 	TIM5_Int_Init(1000-1, 7200-1);              //初始化定时器5， 定时器周期1S
+	TIM6_Int_Init(1000-1, 7200-1);              //初始化定时器6， 定时器周期1S
 	xTaskCreate((TaskFunction_t )start_task,    //任务函数
 							 (const char*   )"start_task",  //任务名称
 							 (uint16_t      )START_STK_SIZE, //任务堆栈大小
@@ -85,31 +87,31 @@ void interrupt_task(void *pvparameters)
 	}
 }
 
-//通用定时器3中断初始化
+//通用定时器2中断初始化
 //这里时钟选择为APB1的2倍， 而APB1位36M
 //arr; 自动重装
 //psc: 时钟预分频数
-//这里使用的是定时器3
-void TIM3_Int_Init(u16 arr, u16 psc)
+//这里使用的是定时器2
+void TIM2_Int_Init(u16 arr, u16 psc)
 {
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
 	NVIC_InitTypeDef NVIC_InitStructure;
 	
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); //时钟使能
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); //时钟使能
 	TIM_TimeBaseStructure.TIM_Period = arr; //自动重装载值
 	TIM_TimeBaseStructure.TIM_Prescaler = psc;
 	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
 	
-	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 5;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 	
-	TIM_Cmd(TIM3, ENABLE);
+	TIM_Cmd(TIM2, ENABLE);
 	//TIM_Cmd(TIM3, DISABLE);
 }
 
@@ -169,13 +171,40 @@ void TIM5_Int_Init(u16 arr, u16 psc)
 	TIM_Cmd(TIM5, ENABLE);
 }
 
-
-void TIM3_IRQHandler(void)
+//通用定时器3中断初始化
+//这里时钟选择为APB1的2倍， 而APB1位36M
+//arr; 自动重装
+//psc: 时钟预分频数
+//这里使用的是定时器6
+void TIM6_Int_Init(u16 arr, u16 psc)
 {
-	if(TIM_GetITStatus(TIM3, TIM_IT_Update)==SET)
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE); //时钟使能
+	TIM_TimeBaseStructure.TIM_Period = arr; //自动重装载值
+	TIM_TimeBaseStructure.TIM_Prescaler = psc;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM6, &TIM_TimeBaseStructure);
+	TIM_ITConfig(TIM6, TIM_IT_Update, ENABLE);
+	
+	NVIC_InitStructure.NVIC_IRQChannel = TIM6_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 4;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	TIM_Cmd(TIM6, ENABLE);
+}
+
+
+void TIM2_IRQHandler(void)
+{
+	if(TIM_GetITStatus(TIM2, TIM_IT_Update)==SET)
 	{
-		printf("TIM3输出......\r\n");
-		TIM_ClearITPendingBit(TIM3, TIM_IT_Update); 
+		printf("TIM2输出......\r\n");
+		TIM_ClearITPendingBit(TIM2, TIM_IT_Update); 
 	}
 }
 
@@ -197,5 +226,16 @@ void TIM5_IRQHandler(void)
 	TIM_ClearITPendingBit(TIM5,TIM_IT_Update);
 		
 }
+
+void TIM6_IRQHandler(void)
+{
+	if(TIM_GetITStatus(TIM6, TIM_IT_Update) == SET)
+	{
+		printf("TIM6输出......\r\n");
+	}
+	TIM_ClearITPendingBit(TIM6,TIM_IT_Update);
+		
+}
+
 
 
